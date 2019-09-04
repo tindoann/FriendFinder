3c745fb5542dbcc92161514485edbe5d3ae94c67
tindoann <tindoann@gamil.com>
2019-09-03 23:14:25 -0400

    setup server

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,5 +16,39 @@ let friendList =
   "4",
   "1"
   ]
+  }, 
+  {
+  "name": "James",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+  "scores": [
+  "5",
+  "2",
+  "4",
+  "7",
+  "5",
+  "9",
+  "2",
+  "2",
+  "3",
+  "1"
+  ]
+  }, 
+  {
+  "name": "Jill",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+  "scores": [
+  "6",
+  "1",
+  "3",
+  "6",
+  "4",
+  "9",
+  "2",
+  "2",
+  "1",
+  "3"
+  ]
   }
-]
+]; 
+
+module.exports = friendList; 
--- app/routing/apiRouters.js
+++ app/routing/apiRouters.js
@@ -1,9 +1,22 @@
-var friends = require("../app/data/friend");
+let friends = require("../data/friends");
 
 module.exports = function(app) {
   app.get('/api/friends', function(req, res) {
     res.json(friends); 
+    console.log(friends); 
   });
+
+  app.get('/api/friends/:friendList', function(req, res) {
+    let chosen = req.params.friends; // storing the friend parameter
+    console.log(chosen);
+
+    for (let i = 0; i < friends.length; i++) {
+      if (chosen === friends[i].routeName)
+    }
+    
+
+
+  })
   
   app.post('/api/friends', function(req, res) {
     friends.push(req.body);
