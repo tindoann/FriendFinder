6fbcb004fcef1c18ba59a9beb38b1cb87acceba4
tindoann <tindoann@gamil.com>
2019-09-11 20:24:38 -0400

    modal working with photo

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,12 +5,12 @@ let friendList = [{
   },
   {
     "name": "James",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
     "scores": ["5", "1", "2", "4", "3", "1", "2", "2", "4", "5"]
   },
   {
     "name": "Jill",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    "photo": "https://get.pxhere.com/photo/person-people-girl-woman-hair-photography-portrait-model-lady-facial-expression-smile-face-eye-skin-beauty-beautiful-blond-pretty-photo-shoot-brown-hair-portrait-photography-914793.jpg",
     "scores": ["1", "3", "4", "2", "3", "5", "2", "5", "4", "2"]
   }
 ];
